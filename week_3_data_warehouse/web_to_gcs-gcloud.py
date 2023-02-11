222abae41dd6ef2167501d03f41d707a3ea95f9e
dherzey <jdtganding0998@outlook.com>
2023-02-11 14:12:01 +0000

    Modified week 3 ETL script

--- week_3_data_warehouse/web_to_gcs-gcloud.py
+++ week_3_data_warehouse/web_to_gcs-gcloud.py
@@ -4,7 +4,6 @@ import pandas as pd
 from pathlib import Path
 from google.cloud import storage
 
-init_url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/fhv/'
 BUCKET = os.environ.get("GCP_GCS_BUCKET", "dtc_data_lake_zoomcamp-user")
 
 def upload_to_gcs(bucket, object_name, local_file):
@@ -21,6 +20,8 @@ def upload_to_gcs(bucket, object_name, local_file):
 
 def web_to_gcs(service, year):
 
+    init_url = f'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{service}/'
+
     # create parent directory if it does not exist
     path_dir = Path(f"data/{service}")
     path_dir.mkdir(parents=True, exist_ok=True)
@@ -29,27 +30,21 @@ def web_to_gcs(service, year):
 
         file_name = f"{service}_tripdata_{year}-{month+1:02}.csv.gz"
         url = init_url + file_name
-        local_file = path_dir/file_name
-
-        # download data as a csv file or just proceed to read it directly
-        os.system(f"wget {url} -O {local_file}")
 
         try:
             # read file and convert to csv/parquet
-            df = pd.read_csv(local_file, compression="gzip")
+            df = pd.read_csv(url)
             file_name = file_name.replace('.csv.gz','.csv')
-            local_file = path_dir/file_name
+            local_file = path_dir/file_name            
             df.to_csv(local_file)
-            # df.to_parquet(local_file, engine='pyarrow')
+            # df.to_parquet(local_file, compression="gzip", engine='pyarrow')
             print(f"Dataset {file_name} read with shape {df.shape}")
         except pd.errors.EmptyDataError:
-            os.remove(local_file)
-            print(f"Removed {local_file}")
-        except FileNotFoundError:
             pass
 
-        # upload parquet file to gcs 
+        # upload file to gcs 
         upload_to_gcs(BUCKET, f"data/{service}/{file_name}", local_file)
         print(f"File uploaded to GCS with path {local_file}")
 
-web_to_gcs('fhv', 2019)
+# web_to_gcs('fhv', 2019)
+web_to_gcs('green', 2019)
